6bb846fe95a2b65bc6e565fb49fa3186193f07bc
mitty1293 <k.fmichi@gmail.com>
2021-04-10 22:40:43 +0900

    post部のひな型作成

---
+++ ts/post_irkit.ts
@@ -0,0 +1,24 @@
+import {SystemConst} form './const.js';
+//DOM
+const light_btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("light_btn");
+const ac_on_btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ac_on_btn");
+const ac_off_btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("ac_off_btn");
+const heater_on_btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("heater_on_btn");
+const heater_off_btn: HTMLButtonElement = <HTMLButtonElement>document.getElementById("heater_off_btn");
+
+//eventlistener
+light_btn.addEventListener('click', () => {
+    
+});
+ac_on_btn.addEventListener('click', () => {
+    
+});
+ac_off_btn.addEventListener('click', () => {
+    
+});
+heater_on_btn.addEventListener('click', () => {
+    
+});
+heater_off_btn.addEventListener('click', () => {
+    
+});
