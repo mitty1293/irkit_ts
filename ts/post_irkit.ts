786816b20073a681a5c307ce90c72a394f40cfff
mitty1293 <k.fmichi@gmail.com>
2021-04-11 10:22:31 +0900

    fetchの記載

--- ts/post_irkit.ts
+++ ts/post_irkit.ts
@@ -8,17 +8,37 @@ const heater_off_btn: HTMLButtonElement = <HTMLButtonElement>document.getElement
 
 //eventlistener
 light_btn.addEventListener('click', () => {
-    
+    fetch(SystemConst.IrkitPara.POST_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(SystemConst.Signals.LIGHT_SIG)
+    })
 });
 ac_on_btn.addEventListener('click', () => {
-    
+    fetch(SystemConst.IrkitPara.POST_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(SystemConst.Signals.AC_ON_SIG)
+    })
 });
 ac_off_btn.addEventListener('click', () => {
-    
+    fetch(SystemConst.IrkitPara.POST_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(SystemConst.Signals.AC_OFF_SIG)
+    })
 });
 heater_on_btn.addEventListener('click', () => {
-    
+    fetch(SystemConst.IrkitPara.POST_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(SystemConst.Signals.HEATER_ON_SIG)
+    })
 });
 heater_off_btn.addEventListener('click', () => {
-    
+    fetch(SystemConst.IrkitPara.POST_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(SystemConst.Signals.HEATER_OFF_SIG)
+    })
 });
